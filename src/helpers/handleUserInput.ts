0f4e3ea2a06cbf26000b8f9fb784264f9ec6e0db
Sarah <sarahgobrien@gmail.com>
2021-11-13 13:59:19 +0100

    Remove console.log

--- src/helpers/handleUserInput.ts
+++ src/helpers/handleUserInput.ts
@@ -4,8 +4,6 @@ import { InputKeys } from "../models/InputKeys";
 export const updatePosition = (event: KeyboardEvent, currentPos:{x:number, y:number}) => {
 
     const key = event.key;
-
-    console.log('CURRENT POS', currentPos);
  
     switch(key){
         case InputKeys.ARROW_UP: return {x: currentPos.x, y: currentPos.y - 10};
